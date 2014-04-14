5b6660fa650292d3102a39d9cc6cb6a82e64fdf1
rtm7777 <kolan678@gmail.com>
2014-04-14 18:56:21 +0300

    added gruntfile, regesign messages window, send on enter

---
+++ public/js/main.js
@@ -0,0 +1,23 @@
+require.config({
+	paths: {
+		'jquery'    : 'bower_components/jquery/dist/jquery',
+		'text'      : 'bower_components/requirejs-text/text',
+		'bootstrap' : 'bower_components/bootstrap/dist/js/bootstrap',
+		'bacon'     : 'bower_components/bacon/dist/Bacon',
+		'draggable' : 'bower_components/draggable/draggable',
+		'underscore': 'bower_components/underscore/underscore',
+		'socket.io' : 'bower_components/socket.io-client/dist/socket.io'
+	},
+	shim: {
+		'bootstrap' : ["jquery"],
+		'bacon'     : ['jquery'],
+		'underscore': ['jquery']
+	}
+});
+
+require([
+	'bootstrap',
+	'controllers/mainController',
+	'controllers/chatListController',
+	'controllers/chatController',
+]);
--- web/js/controllers/chatController.js
+++ web/js/controllers/chatController.js
@@ -1,12 +1,26 @@
 define(['jquery',
 	'../services/socket',
-	'../views/chatView'
+	'../views/chatView',
+	'bacon'
 ], function($, socket, chatView) {
+	function keyCodeIs(keyCode) { 
+		return function(event) { return event.keyCode == keyCode && !event.shiftKey}
+	};
+
 	var $input    = $(".chat_txt_area");
 	var $send_btn = $('#chat_send');
 
+	var sendStream = $send_btn.asEventStream('click');
+	var inputStream = $input.asEventStream('keyup').filter(keyCodeIs(13));
+	var disEnterStream = $input.asEventStream('keydown').filter(keyCodeIs(13));
 
-	$send_btn.on('click', function() {
+	disEnterStream.onValue(function(e) {
+		e.preventDefault();
+	});
+
+	var messagesStream = inputStream.merge(sendStream).filter(function() { return $input.val()});
+
+	messagesStream.onValue(function() {
 		socket.emit('chat', $input.val());
 		$input.val('');
 		return false;

cbea5b412b26f743a2eb639ad528bb642367ecff
rtm7777 <kolan678@gmail.com>
2014-04-22 19:26:33 +0300

    fixed replacing only one smile

--- web/js/services/smiley.js
+++ web/js/services/smiley.js
@@ -1,22 +1,23 @@
 define([''], function() {
 	var emoticons = {
-		':)': '0101.gif',
-		':(': '0102.gif',
-		':D': '0103.gif',
-		'(cool)': '0104.gif',
-		':O': '0105.gif',
-		';)': '0106.gif',
-		';(': '0107.gif',
-		':$': '0204.gif',
-		'(facepalm)': '0406.gif',
-		'(devil)': '0407.gif',
-		'(angel)': '0408.gif',
-		'(rock)': '1106.gif'
+		":\\)"        : '0101.gif',
+		':\\('        : '0102.gif',
+		':D'        : '0103.gif',
+		'\\(cool\\)'    : '0104.gif',
+		':O'        : '0105.gif',
+		';\\)'        : '0106.gif',
+		';\\('        : '0107.gif',
+		':$'        : '0204.gif',
+		'\\(facepalm\\)': '0406.gif',
+		'\\(devil\\)'   : '0407.gif',
+		'\\(angel\\)'   : '0408.gif',
+		'\\(rock\\)'    : '1106.gif'
 	}
 
 	return function(msg) {
 		for (var i in emoticons) {
-			msg = msg.replace(i, "<img src='/img/smiles/" + emoticons[i] + "'>");
+			var re = new RegExp(i, "g");
+			msg = msg.replace(re, "<img src='/img/smiles/" + emoticons[i] + "'>");
 		}
 		
 		return msg;
